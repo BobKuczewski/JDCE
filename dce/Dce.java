c8a06d2784e215180271cc1711aef71715b8ef90
Bob <bob@ushawks.org>
2024-11-09 13:42:46 -0800

    Added Thread.yield per comments provided by Gaston Williams
    Gaston wrote (on COSMAC ELF in groups.io):
    
    Hi Bob,
    
    I believe yield is a new keyword associated with the switch statement.
    From googling, it looks like it was introduced with Java 13.  This new
    keyword collides with previously named references and methods, in
    particular Thread.yield().  So to prevent an ambiguous reference they
    now have to be qualified.
    
    Since Dce extends Thread, you can just use this.yield() to invoke the
    yield method and get rid of the error message. In other cases, one can
    invoke the method with Thread.yield() since it's a public static method
    in the Thread class.
    
    Best regards,
    Gaston

--- dce/Dce.java
+++ dce/Dce.java
@@ -26,7 +26,7 @@ public class Dce extends Thread {
         board.cycle(net);
         try {
           if (down==0) { sleep(1); down=100;  }
-          yield();
+          Thread.yield();
           } catch (InterruptedException e) { }
         }
       }
