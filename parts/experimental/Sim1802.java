51e8419811d198a0f32ce04c50db0c2f23c47e3e
Bob <bob@ushawks.org>
2024-11-15 15:17:00 -0800

    Created new file Sim1802.java to hold the core logic.
    Sim1802.java will hopefully be a stand alone class that can be used
    either inside of JDCE or on its own. It will be used by the part
    CCDP1802BCex.java, but it may also have its own main function.

--- parts/experimental/Sim1802.java
+++ parts/experimental/Sim1802.java
@@ -10,77 +10,62 @@ import shared.*;
 * </pre>
 */
 
-public class CCDP1802BCex extends Ic {
+class pin {
+  int value;
+  public pin() {
+    value = 0;
+  }
+}
 
-  Sim1802 sim;
+public class Sim1802 extends Ic {
+
+  pin p;
 
   // Define pin numbers for this part
-  /*
-  final int pCLK = 1;
-  final int pnWAIT = 2;
-  final int pnCLEAR = 3;
-  final int pQ = 4;
-  final int pSC1 = 5;
-  final int pSC0 = 6;
-  final int pnMRD = 7;
-  final int pDB7 = 8;
-  final int pDB6 = 9;
-  final int pDB5 = 10;
-  final int pDB4 = 11;
-  final int pDB3 = 12;
-  final int pDB2 = 13;
-  final int pDB1 = 14;
-  final int pDB0 = 15;
-  final int pVCC = 16;
-  final int pN2 = 17;
-  final int pN1 = 18;
-  final int pN0 = 19;
-  final int pVSS = 20;
-  final int pnEF4 = 21;
-  final int pnEF3 = 22;
-  final int pnEF2 = 23;
-  final int pnEF1 = 24;
-  final int pMA0 = 25;
-  final int pMA1 = 26;
-  final int pMA2 = 27;
-  final int pMA3 = 28;
-  final int pMA4 = 29;
-  final int pMA5 = 30;
-  final int pMA6 = 31;
-  final int pMA7 = 32;
-  final int pTPB = 33;
-  final int pTPA = 34;
-  final int pnMWR = 35;
-  final int pnINT = 36;
-  final int pnDMAOUT = 37;
-  final int pnDMAIN = 38;
-  final int pnXTAL = 39;
-  final int pVDD = 40;
-  final int nPins = 40;
-  */
+  public final static int pCLK = 1;
+  public final static int pnWAIT = 2;
+  public final static int pnCLEAR = 3;
+  public final static int pQ = 4;
+  public final static int pSC1 = 5;
+  public final static int pSC0 = 6;
+  public final static int pnMRD = 7;
+  public final static int pDB7 = 8;
+  public final static int pDB6 = 9;
+  public final static int pDB5 = 10;
+  public final static int pDB4 = 11;
+  public final static int pDB3 = 12;
+  public final static int pDB2 = 13;
+  public final static int pDB1 = 14;
+  public final static int pDB0 = 15;
+  public final static int pVCC = 16;
+  public final static int pN2 = 17;
+  public final static int pN1 = 18;
+  public final static int pN0 = 19;
+  public final static int pVSS = 20;
+  public final static int pnEF4 = 21;
+  public final static int pnEF3 = 22;
+  public final static int pnEF2 = 23;
+  public final static int pnEF1 = 24;
+  public final static int pMA0 = 25;
+  public final static int pMA1 = 26;
+  public final static int pMA2 = 27;
+  public final static int pMA3 = 28;
+  public final static int pMA4 = 29;
+  public final static int pMA5 = 30;
+  public final static int pMA6 = 31;
+  public final static int pMA7 = 32;
+  public final static int pTPB = 33;
+  public final static int pTPA = 34;
+  public final static int pnMWR = 35;
+  public final static int pnINT = 36;
+  public final static int pnDMAOUT = 37;
+  public final static int pnDMAIN = 38;
+  public final static int pnXTAL = 39;
+  public final static int pVDD = 40;
+  public final static int nPins = 40;
 
   // Define the output pins
-  final int outpins[] = {
-    Sim1802.pQ,
-    Sim1802.pSC1,
-    Sim1802.pSC0,
-    Sim1802.pnMRD,
-    Sim1802.pN2,
-    Sim1802.pN1,
-    Sim1802.pN0,
-    Sim1802.pMA0,
-    Sim1802.pMA1,
-    Sim1802.pMA2,
-    Sim1802.pMA3,
-    Sim1802.pMA4,
-    Sim1802.pMA5,
-    Sim1802.pMA6,
-    Sim1802.pMA7,
-    Sim1802.pTPB,
-    Sim1802.pTPA,
-    Sim1802.pnMWR,
-    Sim1802.pnXTAL
-  };
+  final int outpins[] = {pQ,pSC1,pSC0,pnMRD,pN2,pN1,pN0,pMA0,pMA1,pMA2,pMA3,pMA4,pMA5,pMA6,pMA7,pTPB,pTPA,pnMWR,pnXTAL};
 
   int lastCLK = 0;
   boolean in_init = true;
@@ -98,14 +83,13 @@ public class CCDP1802BCex extends Ic {
   * This method is called by JDCE to build this part.
   * </pre>
   */
-  public CCDP1802BCex() {
+  public Sim1802() {
     super();
-    sim = new Sim1802();
     name = new String("CDP1802BCex");
     pin  = new int[43]; // code below uses 42 ... [41]; // Arrays start at 0, but pins start at 1, so leave room
     lastCLK = 0;
-    pin[sim.pTPA] = 0;
-    pin[sim.pTPB] = 0;
+    pin[pTPA] = 0;
+    pin[pTPB] = 0;
     println ( "Created a " + name );
   }
 
@@ -124,7 +108,7 @@ public class CCDP1802BCex extends Ic {
   */
   public int pinOut(int pn) {
     // What does this section do? What is pin[42]? What are return codes 128 and 129?
-    if (pn>=sim.pDB7 && pn<=sim.pDB0) {
+    if (pn>=pDB7 && pn<=pDB0) {
       if (pin[42]==4) {
         return 129;
       } else {
@@ -154,9 +138,9 @@ public class CCDP1802BCex extends Ic {
     boolean falling_edge = false;
 
     // Check for a clock edge ... everything happens on a clock edge
-    if ( (pin[sim.pCLK] != 0) && (lastCLK == 0) ) rising_edge = true;
-    if ( (pin[sim.pCLK] == 0) && (lastCLK != 0) ) falling_edge = true;
-    lastCLK = pin[sim.pCLK];
+    if ( (pin[pCLK] != 0) && (lastCLK == 0) ) rising_edge = true;
+    if ( (pin[pCLK] == 0) && (lastCLK != 0) ) falling_edge = true;
+    lastCLK = pin[pCLK];
     for (int i=0; i<outpins.length; i++) {
       if (pin[outpins[i]] == 2) {
         pin[outpins[i]] = 0;
@@ -174,31 +158,31 @@ public class CCDP1802BCex extends Ic {
 
       // Set TPA and TPB as needed
       if        ((clk%16) == 1) {
-        pin[sim.pTPA] = 1;
+        pin[pTPA] = 1;
       } else if ((clk%16) == 3) {
-        pin[sim.pTPA] = 0;
+        pin[pTPA] = 0;
       } else if ((clk%16) == 12) {
-        pin[sim.pTPB] = 1;
+        pin[pTPB] = 1;
       } else if ((clk%16) == 14) {
-        pin[sim.pTPB] = 0;
+        pin[pTPB] = 0;
       }
       // Toggle SC0 as needed
       if ((clk%16) == 15) {
-        pin[sim.pSC0] = 1 - pin[sim.pSC0];
+        pin[pSC0] = 1 - pin[pSC0];
       }
 
       // Print the state if debugging is enabled
-      println ( "TPA=" + pin[sim.pTPA] + ", TPB=" + pin[sim.pTPB] + ", SC0=" + pin[sim.pSC0] );
+      println ( "TPA=" + pin[pTPA] + ", TPB=" + pin[pTPB] + ", SC0=" + pin[pSC0] );
 
       // Advance the clock by half a cycle
       clk = clk + 1;
     }
 
     // Output to the nXTAL pin (opposite of CLK)
-    if (pin[sim.pCLK] == 0) {
-      pin[sim.pnXTAL] = 1;
+    if (pin[pCLK] == 0) {
+      pin[pnXTAL] = 1;
     } else {
-      pin[sim.pnXTAL] = 0;
+      pin[pnXTAL] = 0;
     }
 
     return 0;
