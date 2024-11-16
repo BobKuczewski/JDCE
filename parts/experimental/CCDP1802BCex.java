de086690f66a9f4e15e83a54483f4c06e3562e78
Bob <bob@ushawks.org>
2024-11-16 03:58:58 -0800

    Began moving code from CCDP1802BCex.java to Sim1802.java ... works.
    The current simulation only generates TPA, TPB, and SC0. However,
    the Sim1802 program now has its own "main" function so it can be
    run outside of JCDE (with the "package" statement commented out).

--- parts/experimental/CCDP1802BCex.java
+++ parts/experimental/CCDP1802BCex.java
@@ -3,10 +3,9 @@ import parts.*;
 import shared.*;
 
 /**
-* This class/part aspires to be a clock-for-clock simulation
-* of a CDP1802BC microprocessor. At this time, it only
-* generates /XTAL, TPA, TPB, and SC0.
-*
+* <pre>
+* This class/part provides an interface between JDCE
+* and the Sim1802 class.
 * </pre>
 */
 
@@ -14,77 +13,9 @@ public class CCDP1802BCex extends Ic {
 
   Sim1802 sim;
 
-  // Define pin numbers for this part
-  /*
-  final int pCLK = 1;
-  final int pnWAIT = 2;
-  final int pnCLEAR = 3;
-  final int pQ = 4;
-  final int pSC1 = 5;
-  final int pSC0 = 6;
-  final int pnMRD = 7;
-  final int pDB7 = 8;
-  final int pDB6 = 9;
-  final int pDB5 = 10;
-  final int pDB4 = 11;
-  final int pDB3 = 12;
-  final int pDB2 = 13;
-  final int pDB1 = 14;
-  final int pDB0 = 15;
-  final int pVCC = 16;
-  final int pN2 = 17;
-  final int pN1 = 18;
-  final int pN0 = 19;
-  final int pVSS = 20;
-  final int pnEF4 = 21;
-  final int pnEF3 = 22;
-  final int pnEF2 = 23;
-  final int pnEF1 = 24;
-  final int pMA0 = 25;
-  final int pMA1 = 26;
-  final int pMA2 = 27;
-  final int pMA3 = 28;
-  final int pMA4 = 29;
-  final int pMA5 = 30;
-  final int pMA6 = 31;
-  final int pMA7 = 32;
-  final int pTPB = 33;
-  final int pTPA = 34;
-  final int pnMWR = 35;
-  final int pnINT = 36;
-  final int pnDMAOUT = 37;
-  final int pnDMAIN = 38;
-  final int pnXTAL = 39;
-  final int pVDD = 40;
-  final int nPins = 40;
-  */
-
-  // Define the output pins
-  final int outpins[] = {
-    Sim1802.pQ,
-    Sim1802.pSC1,
-    Sim1802.pSC0,
-    Sim1802.pnMRD,
-    Sim1802.pN2,
-    Sim1802.pN1,
-    Sim1802.pN0,
-    Sim1802.pMA0,
-    Sim1802.pMA1,
-    Sim1802.pMA2,
-    Sim1802.pMA3,
-    Sim1802.pMA4,
-    Sim1802.pMA5,
-    Sim1802.pMA6,
-    Sim1802.pMA7,
-    Sim1802.pTPB,
-    Sim1802.pTPA,
-    Sim1802.pnMWR,
-    Sim1802.pnXTAL
-  };
-
   int lastCLK = 0;
   boolean in_init = true;
-  int clk = 14;        // Currently counts half clocks, initialized for proper phasing (not currently counting init)
+  int clk = 14;   // Currently counts half clocks, initialized for proper phasing (not currently counting init)
 
   // Helper function for debugging
   void println ( String s ) {
@@ -115,7 +46,7 @@ public class CCDP1802BCex extends Ic {
   * </pre>
   */
   public int numPins() {
-    return 40;
+    return sim.num_pins;
   }
 
   /**
@@ -132,8 +63,8 @@ public class CCDP1802BCex extends Ic {
       }
     } else {
       // Check if this is an output pin and return 1 if it is.
-      for (int i=0; i<outpins.length; i++) {
-        if (pn == outpins[i]) {
+      for (int i=0; i<sim.outpins.length; i++) {
+        if (pn == sim.outpins[i]) {
           return 1;
         }
       }
@@ -153,53 +84,25 @@ public class CCDP1802BCex extends Ic {
     boolean rising_edge = false;
     boolean falling_edge = false;
 
+    if (Globals.debug_enabled) {
+      sim.enable_debug();
+    } else {
+      sim.disable_debug();
+    }
+
     // Check for a clock edge ... everything happens on a clock edge
     if ( (pin[sim.pCLK] != 0) && (lastCLK == 0) ) rising_edge = true;
     if ( (pin[sim.pCLK] == 0) && (lastCLK != 0) ) falling_edge = true;
-    lastCLK = pin[sim.pCLK];
-    for (int i=0; i<outpins.length; i++) {
-      if (pin[outpins[i]] == 2) {
-        pin[outpins[i]] = 0;
-      }
-    }
     if (rising_edge || falling_edge) {
-
-      if (rising_edge) {
-        // This is a rising clock edge
-        println ( "Rising Edge of clock " + clk );
-      } else if (falling_edge) {
-        // This is a falling clock edge
-        println ( "Falling Edge of clock " + clk );
+      for (int p=0; p<=sim.num_pins; p++) {
+        sim.pin[p] = pin[p];
       }
-
-      // Set TPA and TPB as needed
-      if        ((clk%16) == 1) {
-        pin[sim.pTPA] = 1;
-      } else if ((clk%16) == 3) {
-        pin[sim.pTPA] = 0;
-      } else if ((clk%16) == 12) {
-        pin[sim.pTPB] = 1;
-      } else if ((clk%16) == 14) {
-        pin[sim.pTPB] = 0;
+      sim.run_half_clock();
+      for (int p=0; p<=sim.num_pins; p++) {
+        pin[p] = sim.pin[p];
       }
-      // Toggle SC0 as needed
-      if ((clk%16) == 15) {
-        pin[sim.pSC0] = 1 - pin[sim.pSC0];
-      }
-
-      // Print the state if debugging is enabled
-      println ( "TPA=" + pin[sim.pTPA] + ", TPB=" + pin[sim.pTPB] + ", SC0=" + pin[sim.pSC0] );
-
-      // Advance the clock by half a cycle
-      clk = clk + 1;
     }
-
-    // Output to the nXTAL pin (opposite of CLK)
-    if (pin[sim.pCLK] == 0) {
-      pin[sim.pnXTAL] = 1;
-    } else {
-      pin[sim.pnXTAL] = 0;
-    }
+    lastCLK = pin[sim.pCLK];
 
     return 0;
   }
--- parts/experimental/Sim1802.java
+++ parts/experimental/Sim1802.java
@@ -1,27 +1,39 @@
+// Comment the following line to run as a stand alone class
+
 package parts.experimental;
-import parts.*;
-import shared.*;
+
+
+//import parts.*;
+//import shared.*;
 
 /**
+* <pre>
 * This class/part aspires to be a clock-for-clock simulation
-* of a CDP1802BC microprocessor. At this time, it only
+* of a CDP1802BC microprocessor. It can be built to run both
+* as a stand alone "main" program or as a module within the
+* JDCE simulation environment. At this time, it only
 * generates /XTAL, TPA, TPB, and SC0.
-*
 * </pre>
 */
 
+/*
 class pin {
   int value;
   public pin() {
     value = 0;
   }
 }
+*/
 
-public class Sim1802 extends Ic {
+public class Sim1802 {
 
-  pin p;
+  // Normally inherited from class IC:
+  public int pin[] = new int[100];
+  public String name = "";
+
 
   // Define pin numbers for this part
+  public final static int num_pins = 40;
   public final static int pCLK = 1;
   public final static int pnWAIT = 2;
   public final static int pnCLEAR = 3;
@@ -62,7 +74,6 @@ public class Sim1802 extends Ic {
   public final static int pnDMAIN = 38;
   public final static int pnXTAL = 39;
   public final static int pVDD = 40;
-  public final static int nPins = 40;
 
   // Define the output pins
   final int outpins[] = {pQ,pSC1,pSC0,pnMRD,pN2,pN1,pN0,pMA0,pMA1,pMA2,pMA3,pMA4,pMA5,pMA6,pMA7,pTPB,pTPA,pnMWR,pnXTAL};
@@ -71,12 +82,17 @@ public class Sim1802 extends Ic {
   boolean in_init = true;
   int clk = 14;        // Currently counts half clocks, initialized for proper phasing (not currently counting init)
 
+  boolean debug_enabled = false;
   // Helper function for debugging
   void println ( String s ) {
-    if (Globals.debug_enabled) {
+    //if (Globals.debug_enabled) {
+    if (debug_enabled) {
       System.out.println ( s );
     }
   }
+
+  void enable_debug() { debug_enabled = true; }
+  void disable_debug() { debug_enabled = false; }
 
   /**
   * <pre>
@@ -94,89 +110,38 @@ public class Sim1802 extends Ic {
   }
 
   /**
-  * <pre>
-  * This method is called by JDCE to find out how many pins this part has.
-  * </pre>
-  */
-  public int numPins() {
-    return 40;
-  }
-
-  /**
-  * This method is called by JDCE to figure out which pins are outputs.
-  * This method should return a 1 for output pins and 0 otherwise.
-  */
-  public int pinOut(int pn) {
-    // What does this section do? What is pin[42]? What are return codes 128 and 129?
-    if (pn>=pDB7 && pn<=pDB0) {
-      if (pin[42]==4) {
-        return 129;
-      } else {
-        return 128;
-      }
-    } else {
-      // Check if this is an output pin and return 1 if it is.
-      for (int i=0; i<outpins.length; i++) {
-        if (pn == outpins[i]) {
-          return 1;
-        }
-      }
-      // Otherwise return 0 (not an output pin)
-      return 0;
-    }
-  }
-
-  /**
   * This method is called by JDCE during simulation.
   * <p/>
   * Note that this method may be called many times during
   * a single clock cycle.
   */
-  public int run() {
+  public int run_half_clock() {
     // Run one "iteration" (half clock) of the 1802
-    boolean rising_edge = false;
-    boolean falling_edge = false;
-
-    // Check for a clock edge ... everything happens on a clock edge
-    if ( (pin[pCLK] != 0) && (lastCLK == 0) ) rising_edge = true;
-    if ( (pin[pCLK] == 0) && (lastCLK != 0) ) falling_edge = true;
-    lastCLK = pin[pCLK];
     for (int i=0; i<outpins.length; i++) {
       if (pin[outpins[i]] == 2) {
         pin[outpins[i]] = 0;
       }
     }
-    if (rising_edge || falling_edge) {
-
-      if (rising_edge) {
-        // This is a rising clock edge
-        println ( "Rising Edge of clock " + clk );
-      } else if (falling_edge) {
-        // This is a falling clock edge
-        println ( "Falling Edge of clock " + clk );
-      }
-
-      // Set TPA and TPB as needed
-      if        ((clk%16) == 1) {
-        pin[pTPA] = 1;
-      } else if ((clk%16) == 3) {
-        pin[pTPA] = 0;
-      } else if ((clk%16) == 12) {
-        pin[pTPB] = 1;
-      } else if ((clk%16) == 14) {
-        pin[pTPB] = 0;
-      }
-      // Toggle SC0 as needed
-      if ((clk%16) == 15) {
-        pin[pSC0] = 1 - pin[pSC0];
-      }
-
-      // Print the state if debugging is enabled
-      println ( "TPA=" + pin[pTPA] + ", TPB=" + pin[pTPB] + ", SC0=" + pin[pSC0] );
-
-      // Advance the clock by half a cycle
-      clk = clk + 1;
+    // Set TPA and TPB as needed
+    if        ((clk%16) == 1) {
+      pin[pTPA] = 1;
+    } else if ((clk%16) == 3) {
+      pin[pTPA] = 0;
+    } else if ((clk%16) == 12) {
+      pin[pTPB] = 1;
+    } else if ((clk%16) == 14) {
+      pin[pTPB] = 0;
     }
+    // Toggle SC0 as needed
+    if ((clk%16) == 15) {
+      pin[pSC0] = 1 - pin[pSC0];
+    }
+
+    // Print the state if debugging is enabled
+    println ( "TPA=" + pin[pTPA] + ", TPB=" + pin[pTPB] + ", SC0=" + pin[pSC0] );
+
+    // Advance the clock by half a cycle
+    clk = clk + 1;
 
     // Output to the nXTAL pin (opposite of CLK)
     if (pin[pCLK] == 0) {
@@ -187,4 +152,14 @@ public class Sim1802 extends Ic {
 
     return 0;
   }
+
+  public static void main(String[] args) {
+    System.out.println ( "Running Sim1802 on its own." );
+    Sim1802 sim = new Sim1802();
+    sim.enable_debug();
+    for (int i=0; i<40; i++) {
+      sim.run_half_clock();
+    }
+  }
+
 }
